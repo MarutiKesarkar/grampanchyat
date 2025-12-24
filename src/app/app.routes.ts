8e46eea578517552ee95e29c4e7dbb65e06626f7
Maruti Dattatray Kesarkar <marutidattatraykesarkar@Marutis-MacBook-Air.local>
2025-12-24 19:30:02 +0530

    contact page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { About } from './about/about';
 import { Services } from './services/services';
 import { Places } from './places/places';
 import { Gallery } from './gallery/gallery';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
     { path: '', component: Home, pathMatch: 'full' },
@@ -11,6 +12,7 @@ export const routes: Routes = [
     { path: 'about', component: About, pathMatch: 'full' },
     { path: 'services', component: Services, pathMatch: 'full' },
     { path: 'places', component: Places, pathMatch: 'full' },
-     { path: 'gallery', component: Gallery, pathMatch: 'full' }
-    
+    { path: 'gallery', component: Gallery, pathMatch: 'full' },
+    { path: 'contact', component: Contact, pathMatch: 'full' }
+
 ];
